e75290365ddfde4a989817a88d3bf7cd8a8bbd72
Mark Probst <mark.probst@gmail.com>
2010-06-06 17:58:13 +0200

    Use reify instead of proxy.

--- src/clj/at/ac/tuwien/complang/distributor/distributor.clj
+++ src/clj/at/ac/tuwien/complang/distributor/distributor.clj
@@ -6,18 +6,19 @@
 (defn distributor-server [workers]
   (let [connected-workers (into {} (map (fn [worker] [worker (connect (:host worker) (:port worker))]) workers))
 	worker-loads (agent (into {} (map (fn [worker] [worker 0]) workers)))]
-    (proxy [at.ac.tuwien.complang.distributor.DistributionServer] []
-      (compute [fun args]
-	       (let [loads @worker-loads
-		     sorted (sort-by #(loads %) workers)]
-		 (loop [workers sorted]
-		   (if (empty? workers)
-		     (throw (NotImplementedException.))
-		     (let [worker (first workers)
-			   dist (connected-workers worker)]
-		       (send worker-loads (fn [l] (assoc l worker (inc (l worker)))))
-		       (let [result (apply (worker-function (connected-workers worker) fun (fn [& _] ::local)) args)]
-			 (send worker-loads (fn [l] (assoc l worker (dec (l worker)))))
-			 (if (= result ::local)
-			   (recur (rest workers))
-			   result))))))))))
+    (reify
+     DistributionServer
+     (compute [this fun args]
+	      (let [loads @worker-loads
+		    sorted (sort-by #(loads %) workers)]
+		(loop [workers sorted]
+		  (if (empty? workers)
+		    (throw (NotImplementedException.))
+		    (let [worker (first workers)
+			  dist (connected-workers worker)]
+		      (send worker-loads (fn [l] (assoc l worker (inc (l worker)))))
+		      (let [result (apply (worker-function (connected-workers worker) fun (fn [& _] ::local)) args)]
+			(send worker-loads (fn [l] (assoc l worker (dec (l worker)))))
+			(if (= result ::local)
+			  (recur (rest workers))
+			  result))))))))))
--- src/clj/at/ac/tuwien/complang/distributor/worker.clj
+++ src/clj/at/ac/tuwien/complang/distributor/worker.clj
@@ -2,9 +2,10 @@
   (:import [at.ac.tuwien.complang.distributor DistributionServer NotImplementedException]))
 
 (defn worker-server [funcs]
-  (proxy [at.ac.tuwien.complang.distributor.DistributionServer] []
-    (compute [fun-name args]
-	     (let [fun (funcs fun-name)]
-	       (if fun
-		 (apply fun args)
-		 (throw (NotImplementedException.)))))))
+  (reify
+   DistributionServer
+   (compute [this fun-name args]
+	    (let [fun (funcs fun-name)]
+	      (if fun
+		(apply fun args)
+		(throw (NotImplementedException.)))))))
