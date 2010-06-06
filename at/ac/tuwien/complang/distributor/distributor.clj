599e80dbd89744b263aa54bb486a87833d3432a9
Mark Probst <mark.probst@gmail.com>
2010-06-06 14:33:25 +0200

    Distributor uses client to handle connections.

--- at/ac/tuwien/complang/distributor/distributor.clj
+++ at/ac/tuwien/complang/distributor/distributor.clj
@@ -4,39 +4,20 @@
 
 ;; workers is a seq of maps of the form {:host <host> :port <port>}
 (defn distributor-server [workers]
-  (let [connected-workers (ref {})
-	worker-load (ref {})
-	connect-thread-func (fn [worker]
-			      (loop []
-				(if (contains? @connected-workers worker)
-				  (do
-				    (Thread/sleep 1000)
-				    (recur))
-				  ;; FIXME: catch exceptions for connect
-				  (let [dist (connect (:host worker) (:port worker))]
-				    (dosync
-				     (alter connected-workers assoc worker dist)
-				     (alter worker-load assoc worker 0))))))]
-    (doseq [worker workers]
-      (let [thread (Thread. (fn [] (connect-thread-func worker)))]
-	(.setDaemon thread true)
-	(.start thread)))
+  (let [connected-workers (into {} (map (fn [worker] [worker (connect (:host worker) (:port worker))]) workers))
+	worker-loads (agent (into {} (map (fn [worker] [worker 0]) workers)))]
     (proxy [at.ac.tuwien.complang.distributor.DistributionServer] []
       (compute [fun args]
-	       (let [sorted (sort-by #(get @worker-load % 0) (keys @connected-workers))]
+	       (let [loads @worker-loads
+		     sorted (sort-by #(loads %) workers)]
 		 (loop [workers sorted]
 		   (if (empty? workers)
 		     (throw (Exception. "no workers with that function"))
 		     (let [worker (first workers)
-			   dist (dosync
-				 (let [dist (@connected-workers worker)]
-				   (when dist
-				     (alter worker-load assoc worker (inc (@worker-load worker))))
-				   dist))]
-		       ;; FIXME: catch exceptions
-		       (let [result (.compute dist fun args)]
-			 (dosync
-			  (when (contains? @connected-workers worker)
-			    (alter worker-load assoc worker (dec (@worker-load worker)))))
-			 result)))))))))
-
+			   dist (connected-workers worker)]
+		       (send worker-loads (fn [l] (assoc l worker (inc (l worker)))))
+		       (let [result (apply (worker-function (connected-workers worker) fun (fn [& _] ::local)) args)]
+			 (send worker-loads (fn [l] (assoc l worker (dec (l worker)))))
+			 (if (= result ::local)
+			   (recur (rest workers))
+			   result))))))))))
