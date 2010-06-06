b7e503490160c674fb18960f8239fee7c45c9832
Mark Probst <mark.probst@gmail.com>
2010-06-06 13:25:44 +0200

    Error handling and reconnecting in the client.

--- at/ac/tuwien/complang/distributor/client.clj
+++ at/ac/tuwien/complang/distributor/client.clj
@@ -1,9 +1,40 @@
 (ns at.ac.tuwien.complang.distributor.client)
 
-(defn connect [server port]
-  (let [registry (java.rmi.registry.LocateRegistry/getRegistry server port)]
-    (.lookup registry "Server")))
+(defn- connect-thread-func [host port server-atom]
+  (loop []
+    (if @server-atom
+      (do
+	(Thread/sleep 1000)
+	(recur))
+      (do
+	(try
+	 (let [registry (java.rmi.registry.LocateRegistry/getRegistry host port)
+	       server (.lookup registry "Server")]
+	   (reset! server-atom server))
+	 (catch java.rmi.RemoteException _
+	   (Thread/sleep 1000))
+	 (catch java.rmi.NotBoundException _
+	   (Thread/sleep 1000)))
+	(recur)))))
 
-(defn worker-function [distributor name local-fun]
+(defn connect [host port]
+  (let [server-atom (atom nil)
+	thread (Thread. (fn [] (connect-thread-func host port server-atom)))]
+    (.setDaemon thread true)
+    (.start thread)
+    server-atom))
+
+(defn worker-function [server-atom name local-fun]
   (fn [& args]
-    (.compute distributor name args)))
+    (loop []
+      (let [server @server-atom]
+	(if server
+	  (let [result (try
+			(.compute server name args)
+			(catch java.rmi.ConnectException _
+			  (reset! server-atom nil)
+			  ::failure))]
+	    (if (= result ::failure)
+	      (recur)
+	      result))
+	  (apply local-fun args))))))
