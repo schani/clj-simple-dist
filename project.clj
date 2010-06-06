470caa8003e92dc9662ea9843410dac05c4fe81e
Mark Probst <mark.probst@gmail.com>
2010-06-06 16:13:05 +0200

    Use leiningen.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject dist "0.0.1-SNAPSHOT"
+  :description "Simple Clojure Distribution Service"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :source-path "src/clj"
+  :java-source-path "src/java"
+  :dev-dependencies [[lein-javac "0.0.2-SNAPSHOT"]])
