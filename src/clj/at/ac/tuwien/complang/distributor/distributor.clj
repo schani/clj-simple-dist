d07436d1d893162f2a0c5b322aed1d62590716b0
Mark Probst <mark.probst@gmail.com>
2010-06-08 23:42:57 +0200

    Atomically fetch and update the worker map.

--- src/clj/at/ac/tuwien/complang/distributor/distributor.clj
+++ src/clj/at/ac/tuwien/complang/distributor/distributor.clj
@@ -1,6 +1,7 @@
 (ns at.ac.tuwien.complang.distributor.distributor
   (:import [at.ac.tuwien.complang.distributor Distributor NotImplementedException])
-  (:use at.ac.tuwien.complang.distributor.client))
+  (:use at.ac.tuwien.complang.distributor.client
+	clojure.set))
 
 (defn- make-counter []
   (let [c (atom 0)]
@@ -9,40 +10,52 @@
 (defprotocol DistributorAccess
   (loads [this]))
 
-(defn- add-load [workers name load-id fun time]
-  (let [worker (workers name)]
-    ;; the worker might have been removed already
-    (if worker
-      (let [new-worker (assoc worker :loads (assoc (:loads worker) load-id {:function fun :start-time time}))]
-	(assoc workers name new-worker))
-      workers)))
+(defn- select-worker! [workers-ref exclude load-id fun]
+  (dosync
+   (let [workers @workers-ref
+	 names (difference (set (keys workers)) exclude)]
+     (if (empty? names)
+       nil
+       (let [sorted (sort-by #(count (:loads (workers %))) names)
+	     name (first sorted)
+	     worker (workers name)]
+	 (ref-set workers-ref (assoc workers name (assoc worker :loads (assoc (:loads worker) load-id {:function fun :start-time (System/currentTimeMillis)}))))
+	 [name (workers name)])))))
 
-(defn- remove-load [workers name load-id]
-  (let [worker (workers name)]
-    (if worker
-      (let [new-worker (assoc worker :loads (dissoc (:loads worker) load-id))]
-	(assoc workers name new-worker))
-      workers)))
+(defn- remove-load! [workers-ref name load-id]
+  (dosync
+   (let [workers @workers-ref
+	 worker (workers name)]
+     (when worker
+       (let [new-worker (assoc worker :loads (dissoc (:loads worker) load-id))]
+	 (alter workers-ref assoc name new-worker))))))
+
+(defn- remove-worker! [workers-ref name dist]
+  (dosync
+   (let [workers @workers-ref
+	 worker (workers name)]
+     ;; the worker object must be the same - a different worker with
+     ;; the same name might have been removed and added again
+     (when (and worker (= dist (:worker worker)))
+       (alter workers-ref dissoc name)))))
 
 ;; workers maps from name to a map {:worker <worker-obj> :loads <load-map>}
 ;; where load-map maps from the load-id to a map {:function <fun-name> :start-time <time>}
 (defn distributor-server []
-  (let [workers (agent {})
+  (let [workers (ref {})
 	counter (make-counter)]
     (reify
      Distributor
      (register [this worker name]
-	       (send workers assoc name {:worker worker :loads {}}))
+	       (dosync
+		(alter workers assoc name {:worker worker :loads {}})))
      (compute [this fun args]
-	      (let [sorted (sort-by #(count (:loads %)) @workers)]
-		(loop [sorted sorted]
-		  (if (empty? sorted)
-		    (throw (NotImplementedException.))
-		    (let [[name worker] (first sorted)
-			  dist (:worker worker)
-			  load-id (counter)]
-		      (send workers add-load name load-id fun (System/currentTimeMillis))
-		      (let [result (try
+	      (let [load-id (counter)]
+		(loop [exclude #{}]
+		  (let [[name worker] (select-worker! workers exclude load-id fun)]
+		    (if worker
+		      (let [dist (:worker worker)
+			    result (try
 				    (.compute dist fun args)
 				    (catch NotImplementedException exc
 				      ::not-implemented)
@@ -51,15 +64,16 @@
 				    (catch Exception _
 				      ::failure)
 				    (finally
-				     (send workers remove-load name load-id)))]
+				     (remove-load! workers name load-id)))]
 			(case result
 			      ::not-implemented
-			      (recur (rest sorted))
+			      (recur (conj exclude name))
 			      ::failure
 			      (do
-				(send workers dissoc name)
-				(recur (rest sorted)))
-			      result)))))))
+				(remove-worker! workers name dist)
+				(recur (conj exclude name)))
+			      result))
+		      (throw (NotImplementedException.)))))))
      DistributorAccess
      (loads [this]
 	    (into {} (map (fn [kv] [(key kv) (:loads (val kv))])
