e3360d29483d87d081cf862ff37630fb2bf6939d
Mark Probst <mark.probst@gmail.com>
2010-06-12 17:14:04 +0200

    Automatic VSC registration.

--- demo/runworker.sh
+++ demo/runworker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec java -Dpid=$$ -cp `echo lib/*.jar | sed -e 's/ /:/g'`:classes:src/clj clojure.main -i demo/testworker.clj
+exec java -Dat.ac.tuwien.complang.distributor.worker=true -Dpid=$$ -cp `echo lib/*.jar | sed -e 's/ /:/g'`:classes:src/clj clojure.main -i demo/testworker.clj
--- src/clj/at/ac/tuwien/complang/distributor/vsc.clj
+++ src/clj/at/ac/tuwien/complang/distributor/vsc.clj
@@ -1,9 +1,23 @@
 (ns at.ac.tuwien.complang.distributor.vsc
   (:use clojure.contrib.def
-	at.ac.tuwien.complang.distributor.client)
+	at.ac.tuwien.complang.distributor.client
+	at.ac.tuwien.complang.distributor.convenience)
   (:import [java.rmi.server RMISocketFactory]))
 
+(defvar- is-worker (System/getProperty "at.ac.tuwien.complang.distributor.worker"))
+(defvar- server (or (System/getProperty "at.ac.tuwien.complang.distributor.vsc-server") "localhost"))
+
 (defvar- connection (connect "localhost" 1099))
+
+(defvar- functions (atom {}))
+
+(defn vsc-add-fn [name version func]
+  (swap! functions assoc {:name name :version version} func))
+
+(defn vsc-register-worker []
+  (println (str @functions))
+  (register-worker server (str (.getHostName (java.net.InetAddress/getLocalHost)) "-" (System/getProperty "pid"))
+		   @functions))
 
 (defmacro vsc-fn [name version args & body]
   (let [name-string (str name)
@@ -13,8 +27,8 @@
     `(do
        (defn ~name ~args
 	 ~@body)
-       (def ~fn-version-name ~version)
-       (def ~vsc-fn-name (worker-function ~conn {:name ~name-string :version ~version} ~name)))))
+       (def ~vsc-fn-name (worker-function ~conn {:name ~name-string :version ~version} ~name))
+       (vsc-add-fn ~name-string ~version ~name))))
 
 (defvar- host-map
   {"vsc.tuwien.ac.at" "localhost"
@@ -25,8 +39,10 @@
   (let [factory (RMISocketFactory/getDefaultSocketFactory)]
     (proxy [RMISocketFactory] []
       (createServerSocket [port]
-			  (.createServerSocket factory))
+			  (.createServerSocket factory port))
       (createSocket [host port]
 		    (.createSocket factory (or (host-map host) host) port)))))
 
-(RMISocketFactory/setSocketFactory vsc-socket-factory)
+(if is-worker
+  (println "I'm a worker")
+  (RMISocketFactory/setSocketFactory vsc-socket-factory))
